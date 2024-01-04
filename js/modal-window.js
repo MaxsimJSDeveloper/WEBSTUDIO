5438865d340f2a074faafb418d296e9a50274bc2
MaxsimJSDeveloper <147420858+MaxsimJSDeveloper@users.noreply.github.com>
2024-01-04 20:57:34 +0200

    js

--- js/modal-window.js
+++ js/modal-window.js
@@ -1,17 +1,17 @@
 const cssClasses = {
   formButton: 'button',
-  modal: 'modal-window-container',
+  modalWindow: 'modal-window-container',
   closeButton: 'close-button',
   toggleClass: 'is-open',
 };
 
-const burgerMenu = document.querySelector(`.${cssClasses.formButton}`);
-const modal = document.querySelector(`.${cssClasses.modal}`);
+const formMenu = document.querySelector(`.${cssClasses.formButton}`);
+const modal = document.querySelector(`.${cssClasses.modalWindow}`);
 const closeButton = document.querySelector(`.${cssClasses.closeButton}`);
 const menuButtons = document.querySelectorAll(`.${cssClasses.menuButtons}`);
 
 const toggle = () => modal.classList.toggle(cssClasses.toggleClass);
 
-burgerMenu.addEventListener('click', toggle);
+formMenu.addEventListener('click', toggle);
 closeButton.addEventListener('click', toggle);
 menuButtons.forEach(button => button.addEventListener('click', toggle));
