bf5e58f42e7812b416826d0358025ce9ebdcb091
MaxsimJSDeveloper <147420858+MaxsimJSDeveloper@users.noreply.github.com>
2024-03-08 10:29:30 +0200

    js

--- js/modal-window.js
+++ js/modal-window.js
@@ -5,13 +5,73 @@ const cssClasses = {
   toggleClass: 'is-open',
 };
 
+// Відкрити закрити форму
 const formMenu = document.querySelector(`.${cssClasses.formButton}`);
 const modal = document.querySelector(`.${cssClasses.modalWindow}`);
 const closeButton = document.querySelector(`.${cssClasses.closeButton}`);
-const menuButtons = document.querySelectorAll(`.${cssClasses.menuButtons}`);
+const menuButtons = document.querySelectorAll(`.${cssClasses.formButton}`);
 
 const toggle = () => modal.classList.toggle(cssClasses.toggleClass);
 
 formMenu.addEventListener('click', toggle);
 closeButton.addEventListener('click', toggle);
 menuButtons.forEach(button => button.addEventListener('click', toggle));
+
+const form = document.querySelector('.modal-form');
+
+// Надіслати данні з форми
+form.addEventListener('input', saveFormData);
+
+function saveFormData() {
+  const formData = {
+    name: form.elements.name.value.trim(),
+    phone: form.elements.phone.value.trim(),
+    email: form.elements.email.value.trim(),
+    comment: form.elements.comment.value.trim(),
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+window.addEventListener('load', loadFormData);
+
+function loadFormData() {
+  const savedData = localStorage.getItem('feedback-form-state');
+  if (savedData) {
+    const { name, phone, email, comment } = JSON.parse(savedData);
+    form.elements.email.value = email;
+    form.elements.phone.value = phone;
+    form.elements.name.value = name;
+    form.elements.comment.value = comment;
+  }
+}
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  localStorage.removeItem('feedback-form-state');
+
+  const emailValue = form.elements.email.value.trim();
+  const nameValue = form.elements.name.value.trim();
+  const phoneValue = form.elements.phone.value.trim();
+  const commentValue = form.elements.comment.value.trim();
+
+  if (
+    emailValue === '' ||
+    nameValue === '' ||
+    phoneValue === '' ||
+    commentValue === ''
+  ) {
+    alert('Please enter your email and message!');
+    return;
+  }
+
+  console.log({
+    name: nameValue,
+    phone: phoneValue,
+    email: emailValue,
+    comment: commentValue,
+  });
+
+  form.reset();
+}
